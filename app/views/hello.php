5f12e0fe6548f9eac11beab7c5867930afbec06e
Renan Jhonatha ^-^ <renan@proesc.com>
2017-09-09 15:25:21 -0300

    { ajuste } criado view

--- app/views/hello.php
+++ app/views/hello.php
@@ -31,11 +31,6 @@
 	<div class="col-md-2"></div>
 </body>
 
-
-
-
-
-
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
